b03db211322915cafe0d2b25cab0aab48d635f91
i.sumin <i.sumin@tinkoff.ru>
2022-08-10 16:20:39 +0300

    Add project files.

---
+++ TestTask/Auto.cs
@@ -0,0 +1,82 @@
+﻿// Метод, который на основе параметров количества топлива и заданного расстояния вычисляет за сколько автомобиль его преодолеет. - тут
+// скорее всего имеется ввиду на основе параметров средней скорости и заданного расстояния. Сделал так, видимо, очепятка в задании.
+
+namespace TestTask
+{
+    public class Auto
+    {
+        protected float averageConsumption { get; set; }
+        protected int fTankVolume { get; set; }
+        protected int? fuelLeft { get; set; }
+        protected int avSpeed { get; set; }
+        protected readonly string type;
+
+        protected Auto(int avSpeed, int fTankVolume, float averageConsumption, int? fuelLeft, string type)
+        {
+            if (fTankVolume < fuelLeft)
+            {
+                throw new Exception("\'Fuel left\' volume can not be higher than \'Full tank volume\'");
+            }
+
+            this.averageConsumption = averageConsumption;
+            this.fTankVolume = fTankVolume;
+            this.avSpeed = avSpeed;
+            this.fuelLeft = fuelLeft;
+            this.type = type;
+        }
+        public virtual float Range()
+        {
+            return (fuelLeft == null ? fTankVolume : (int)fuelLeft) / (averageConsumption / 100); 
+        }
+        public string timeEstimated(int range)
+        {
+            return "Range: " + range + ". Estimated time: " + ((float)range / avSpeed).ToString() + " hours.";
+        }
+    }
+    public class Sportcar : Auto
+    {
+        public Sportcar(int avSpeed, float averageConsumption, int? fuelLeft) : base(avSpeed,80,averageConsumption,fuelLeft, "Sportcar")
+        {
+        }
+
+        public override float Range()
+        {
+            return base.Range();
+        }
+    }
+    public class CargoBob : Auto
+    {
+        public int capacity { get; set; }
+        public CargoBob(int avSpeed, float averageConsumption, int? fuelLeft, int capacity) : base(avSpeed, 400, averageConsumption, fuelLeft, "Cargo")
+        {
+            if (capacity >= 20000)
+            {
+                throw new Exception("Maximum capacity is 20 tons (20k kgs)");
+            }
+
+            this.capacity = capacity;
+        }
+
+        public override float Range()
+        {
+            return base.Range() / 100 * (100 - (4*(capacity/200)));
+        }
+    }
+    public class PassengerCar : Auto
+    {
+        public int passNumber { get; set; }
+        public PassengerCar(int avSpeed, float averageConsumption, int? fuelLeft, int passNumber) : base(avSpeed, 70, averageConsumption, fuelLeft, "Passenger Car")
+        {
+            if(passNumber >= 5)
+            {
+                throw new Exception("Maximum passenger number is 4");
+            }
+
+            this.passNumber = passNumber;
+        }
+        public override float Range()
+        {
+            return base.Range() / 100 * (100 - (6 * passNumber));
+        }
+    }
+}
---
+++ TestTask/Program.cs
@@ -0,0 +1,108 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TestTask;
+
+Console.WriteLine("Hello, World!");
+
+bool finish = false;
+
+ConsoleKeyInfo choice, scenario;
+
+while(!finish)
+{
+    Console.WriteLine("Choose car type(1, 2 or 3). Tap F to finish:\n1. Sportcar\n2. Cargo car\n3. Passenger car");
+    choice = Console.ReadKey();
+    Console.WriteLine("\n");
+
+    if(choice.Key.ToString()!= "D1" && choice.Key.ToString() != "D2" && choice.Key.ToString() != "D3" && choice.Key.ToString() != "F")
+    {
+
+        Console.WriteLine("Enter valid car type!");
+        continue;
+    }
+    if (choice.Key.ToString() == "F")
+    {
+        finish = true;
+        continue;
+    }
+
+    int range = 0;
+
+    while(true)
+    {
+        Console.WriteLine("\nChoose what you want to calculate:\n1. Range\n2. Time Estimated");
+        scenario = Console.ReadKey();
+
+        if (scenario.KeyChar.ToString() == "1" || scenario.KeyChar.ToString() == "2")
+        {
+            if(scenario.KeyChar.ToString() == "2")
+            {
+                Console.WriteLine("Enter Range to calculate: ");
+                Int32.TryParse(Console.ReadLine(), out range);
+            }
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Choose 1 or 2");
+        }
+    }
+
+    Console.WriteLine("Enter params:\nAverage Speed:");
+    Int32.TryParse(Console.ReadLine(), out int avSpeed);
+    Console.WriteLine("Enter params:\nAverage Consumption:");
+    float.TryParse(Console.ReadLine(), out float averageConsumption);
+    Console.WriteLine("Enter params:\nFuel left (Press Enter to calculate with Full Tank):");
+    var enterRes = Int32.TryParse(Console.ReadLine(), out int fuelLeft);
+
+    switch (choice.Key.ToString())
+    {
+        case "D1":
+            
+            try
+            {
+                var a = new Sportcar(avSpeed, averageConsumption, enterRes ? fuelLeft : null);
+                Console.WriteLine(scenario.KeyChar.ToString() == "1" ? "Range is: " + a.Range() : "Time Estimated: " + a.timeEstimated(range));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            break;
+        case "D2":
+
+            Console.WriteLine("Enter planned Capacity:");
+
+            Int32.TryParse(Console.ReadLine(), out int capacity);
+            try
+            {
+                var b = new CargoBob(avSpeed, averageConsumption, enterRes ? fuelLeft : null, capacity);
+                Console.WriteLine(scenario.KeyChar.ToString() == "1" ? "Range is: " + b.Range() : "Time Estimated: " + b.timeEstimated(range));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            break;
+        case "D3":
+            Console.WriteLine("Enter planned number of passengers:");
+
+            Int32.TryParse(Console.ReadLine(), out int passengers);
+            try
+            {
+                var b = new PassengerCar(avSpeed, averageConsumption, enterRes ? fuelLeft : null, passengers);
+                Console.WriteLine(scenario.KeyChar.ToString() == "1" ? "Range is: " + b.Range() : "Time Estimated: " + b.timeEstimated(range));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+        default:
+            Console.WriteLine("Enter valid car type!");
+            break;
+    }
+}
+
